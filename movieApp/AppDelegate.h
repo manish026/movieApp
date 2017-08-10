8a15173cf017a5f541c417239397f1c6f0f438f5
manish <manish026@live.com>
2017-08-10 21:34:09 +0530

    Initial Commit

---
+++ movieApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  movieApp
+//
+//  Created by Manish on 10/08/17.
+//  Copyright © 2017 Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ movieApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  movieApp
+//
+//  Created by Manish on 10/08/17.
+//  Copyright © 2017 Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
